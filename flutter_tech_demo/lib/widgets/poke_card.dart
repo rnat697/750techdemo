b0f08d04733a8e865c5e6ce1e04b5f8851415ae9
rnat697 <rnat697@aucklanduni.ac.nz>
2023-04-03 00:10:59 +1200

    Added comments on navigator and gesture detection

--- flutter_tech_demo/lib/widgets/poke_card.dart
+++ flutter_tech_demo/lib/widgets/poke_card.dart
@@ -1,8 +1,8 @@
-// ignore_for_file: file_names
+
 import 'package:flutter/material.dart';
 import 'package:flutter_tech_demo/pages/pokemon_info_page.dart';
 import '../utils/Styles.dart';
-
+// This is a custom widget to show each pokemon in a card like style.
 class PokemonCard extends StatelessWidget{
   const PokemonCard({super.key, required this.pokeID, required this.pokeName, required this.spriteImg});
   final int pokeID;
@@ -12,20 +12,24 @@ class PokemonCard extends StatelessWidget{
   @override
   Widget build(BuildContext context){
 
-    return GestureDetector(
+    return GestureDetector( // We wrap the entire widget with a gesture detector to allow users to tap on a pokemon card to view the pokemon's detailed information.
       onTap: () => {
-        Navigator.of(context).push(
+        Navigator.of(context).push( // This one way to navigate to pages / routes. Pushes the widget (page) on to the top of the navigation stack.
           MaterialPageRoute(
             builder: (context) => PokemonInfoPage(id: pokeID, name: pokeName),
           ),
         )
       },
+      // ----------- Card layout -----------
       child: Column(
         children: [
+          // -------- Image of Pokemon Widget ----------
           Expanded(
             child: Image.network(spriteImg, fit: BoxFit.fill),
           ),
+
           const Divider(thickness: 1.5),
+          // ------ Name of pokemon widget --------
           Text(
             "${pokeName[0].toUpperCase()}${pokeName.substring(1)}",
             style: Styles.cardNamesStyle,
